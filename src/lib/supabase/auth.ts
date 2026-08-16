2dd0fe9ef720494182ab604ebfd774c531e7bcac
17reaz <17reaz@gmail.com>
2026-08-16 17:47:02 +0600

    ok

--- src/lib/supabase/auth.ts
+++ src/lib/supabase/auth.ts
@@ -13,7 +13,7 @@ export async function signInWithGoogle() {
   return await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: window.location.origin,
+      redirectTo: `${window.location.origin}/app`,
     },
   });
 }
